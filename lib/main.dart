94c1b5bff25c39fd1697ed9b838e56d64cf7912a
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2022-10-18 06:24:36 -0500

    [ADD] Initial Project

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'H-Company',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() {
+    return _MyHomePageState();
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _selected = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("H-Company"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                _displayDialog(context);
+              },
+              child: Text("Integrantes del equipo"),
+            ),
+            Text(_selected)
+          ],
+        ),
+      ),
+    );
+  }
+
+  _displayDialog(BuildContext context) async {
+    _selected = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Expanded(
+          child: SimpleDialog(
+            title: Text('Integrantes'),
+            children: [
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, "Juan Jesus Vazquez Lozoria");
+                },
+                child: const Text('Juan Jesus Vazquez Lozoria'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  Navigator.pop(context, "example");
+                },
+                child: const Text('example'),
+              ),
+            ],
+            elevation: 10,
+            //backgroundColor: Colors.green,
+          ),
+        );
+      },
+    );
+
+    if (_selected != null) {
+      setState(() {
+        _selected = _selected;
+      });
+    }
+  }
+}
